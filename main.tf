98fef8c05bf3e02df9dc6132e011f1412f976988
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2022-05-24 16:50:06 +1000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features  {}
+}
+
+terraform {
+  backend "azurerm" {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rgmultipleappservicedemo"
+  location = "australiaeast"
+}
+
+resource "azurerm_app_service_plan" "app-plan-linux" {  
+  name                = "necdemoappsvcplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  count = 3
+  name                = "demo-app-service${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app-plan-linux.id
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+}
